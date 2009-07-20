bfb5068e9c5b4318b9d1ca262e04306d141307ea
Michael Leung <blamemike@gmail.com>
2009-07-20 00:24:19 -0400

    Updated commented about silence_warnings

--- lib/koujou/builder.rb
+++ lib/koujou/builder.rb
@@ -25,7 +25,7 @@ module Koujou #:nodoc:
         protected
           def generate_instance(create, attributes)
             # There were a metric ton of AR warnings about instance vars
-            # not being initialized. This hides those.
+            # not being initialized when running our specs. This hides those.
             silence_warnings do
               instance = build_model_instance(self, attributes)
               instance.save! if create 
